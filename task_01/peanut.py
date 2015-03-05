1333d210847c56dc70ff65c7dffb0986e1b0991a
Erica Liz <erica.liz@spsmail.cuny.edu>
2015-03-05 03:17:01 +0000

    created new package folder

---
+++ task_01/peanut.py
@@ -0,0 +1,17 @@
+#! usr/bin/env python
+# *-* coding: utf-8 *-*
+"""Creating a package"""
+
+import task_01
+
+BUTTER = True
+OIL = False
+
+#if __name__ == '__main__':
+ #   print 'I am truthy'
+
+#if __name__ == '__main__':
+ #   print 'I am Falsy'
+
+if task_01.peanut.BUTTER: print 'I am truthy'
+
